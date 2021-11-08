6b15435da8cd7222d1c83e1b48922110a8695121
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2021-11-08 17:17:17 +0530

    dishes added on home page

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,61 @@ let nav_div = document.getElementById("nav_div");
 console.log(navbar);
 
 nav_div.innerHTML = navbar()
+
+
+
+
+
+let res = fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+
+.then((res)=>{
+    return res.json()
+})
+
+.then((res)=>{
+    console.log(res.categories);
+
+    let data = res.categories
+
+    append(data)
+})
+
+.catch((e)=>{
+    console.log(e);
+})
+
+let dish_container = document.getElementById("dish_container")
+
+
+
+function append(data){
+
+    data.forEach((dish) => {
+        let dish_div = document.createElement("div");
+
+        let img = document.createElement("img");
+
+        let mrp = +(dish.idCategory) * 100
+
+        let price = document.createElement("p");
+
+        price.textContent = `price : ${mrp}`
+
+        let btn = document.createElement("button")
+
+        btn.textContent = "Add to cart"
+
+        let name = document.createElement("p")
+
+         name.textContent = dish.strCategory;
+
+        img.src = dish.strCategoryThumb;
+
+        dish_div.append(img);
+
+        dish_container.append(dish_div)
+    });
+
+
+
+}
